4c9960ab76aceaab2064c699bfe22124bcb51883
Kishore Mukherjee <studyutilsk@gmail.com>
2022-01-20 16:33:20 -0800

    Second commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DeployToAzure.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
